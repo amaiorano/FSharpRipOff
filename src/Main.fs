6f5d54a5df5e517d884526d69244b5457ba98874
Antonio Maiorano <amaiorano@gmail.com>
2016-05-27 08:36:17 -0400

    Add solution with OpenTK support

---
+++ src/Main.fs
@@ -0,0 +1,83 @@
+﻿#if !INTERACTIVE
+module Main
+open OpenTKPlatform
+#endif
+
+//#if INTERACTIVE
+//#load "OpenTKPlatform.fs"
+//#endif
+
+let degToRad degs = float(degs) * System.Math.PI / 180.0
+
+type Visual = {
+    vertices:(float*float) list
+}
+
+type Actor = {
+    pos:(float*float)
+    angle:float
+    visual:Visual
+}
+
+let defaultActor = {
+    pos = (0.,0.)
+    angle = 0.
+    visual = { vertices = [] }
+}
+
+let drawVisual (canvas:Canvas) (vis:Visual) =
+    canvas.drawVertices vis.vertices
+
+let drawActor (canvas:Canvas) (actor:Actor) =
+    canvas.save ()
+    canvas.translate actor.pos
+    canvas.rotate (degToRad actor.angle)
+    drawVisual canvas actor.visual
+    canvas.restore ()
+
+let makeShipVisual () =
+    let scale = 10.
+    let verts = [ (-1.,-1.); (0.,2.); (1.,-1.) ] |> List.map (fun (x,y) -> (x*scale, y*scale))
+    { vertices=verts }
+
+let add ((p1x,p1y):float*float) ((p2x,p2y):float*float) =
+    (p1x + p2x, p1y + p2y)
+
+
+let rotateActor degs actor = {actor with angle = actor.angle + degs}
+
+let rec update (app:Application) (actors:Actor list) dt (canvas:Canvas) =
+    // Logic update
+    let player = actors.[0]
+//    let player = { player with pos = add player.pos (keyboard.arrows()) }
+    let others = List.tail actors |> List.map (rotateActor 100.)
+    let actors = player :: others
+    // To keep it pure, we need to re-register a new instance of update that
+    // binds the udpated actors list
+    app.setOnUpdate (update app actors)
+
+    // Render
+    canvas.resetTransform ()
+    canvas.clear ()
+    actors |> List.iter (fun (actor) -> drawActor canvas actor)    
+
+
+let main () =
+    let ship = {
+        defaultActor with 
+            pos = (100.,100.)
+            angle = 180.
+            visual = makeShipVisual ()
+    }
+
+    let actors = [
+        {ship with pos=(50.,50.); angle=20.}
+        {ship with pos=(150.,150.); angle=45.}
+        {ship with pos=(250.,250.); angle=95.}
+    ]
+    
+    let app = new Application("FSharpRipOff")
+    app.setOnUpdate (update app actors)
+    app.run ()
+
+main ()
---
+++ src/OpenTKPlatform.fs
@@ -0,0 +1,86 @@
+﻿#if !INTERACTIVE
+module OpenTKPlatform
+#endif
+
+#if INTERACTIVE
+#r "external/OpenTK/OpenTK.dll"
+#endif
+
+open OpenTK
+open OpenTK.Graphics
+open OpenTK.Graphics.OpenGL
+open OpenTK.Input
+
+type Canvas() =
+    member this.resetTransform () =
+        GL.MatrixMode(MatrixMode.Modelview)
+        GL.LoadIdentity()        
+    member this.clear() = 
+        GL.Clear(ClearBufferMask.ColorBufferBit ||| ClearBufferMask.DepthBufferBit)
+    member this.save () = GL.PushMatrix()
+    member this.restore () = GL.PopMatrix()
+    member this.translate (x:float, y:float) = GL.Translate(x,y,0.)
+    member this.rotate angle = GL.Rotate(angle, Vector3d.UnitZ)    
+    member this.drawVertices (verts:(float*float) list) =
+        GL.Begin(BeginMode.LineLoop)
+        GL.Color3(1.f, 1.f, 1.f);
+        verts |> Seq.iter GL.Vertex2
+        GL.End()
+
+type GameWindow(title) =
+    inherit OpenTK.GameWindow()
+    let canvas = Canvas() //@TODO: move to Application
+    let mutable onUpdate = fun dt c -> ()
+    do
+        base.VSync <- VSyncMode.On
+        base.Title <- title
+
+    member this.setOnUpdate onUpdate' = onUpdate <- onUpdate'
+            
+    override this.OnLoad e =
+        base.OnLoad(e);
+        GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f)
+        GL.Enable(EnableCap.DepthTest)
+
+    override this.OnUpdateFrame e =
+        base.OnUpdateFrame e
+        if Keyboard.GetState().[Key.Escape] then base.Close()
+//        onUpdate e.Time canvas
+
+    override this.OnRenderFrame e =
+        base.OnRenderFrame e
+        GL.Clear(ClearBufferMask.ColorBufferBit ||| ClearBufferMask.DepthBufferBit)
+        GL.MatrixMode(MatrixMode.Modelview)
+        GL.LoadIdentity()
+        onUpdate e.Time canvas
+        base.SwapBuffers()
+
+    override this.OnResize e =
+        base.OnResize e
+        let rect = base.ClientRectangle
+        GL.Viewport(rect.X, rect.Y, rect.Width, rect.Height)
+        let mutable projection = Matrix4.CreateOrthographicOffCenter(0.f, float32 rect.Width, float32 rect.Height, 0.f, -100.f, 100.f)
+        GL.MatrixMode(MatrixMode.Projection)
+        GL.LoadMatrix(&projection)
+
+let setGameWindowSize width height (gameWindow:GameWindow) =
+    gameWindow.Width <- width
+    gameWindow.Height <- height
+    gameWindow
+
+let setGameWindowSizeScreenRatio ratio (gameWindow:GameWindow) =
+    let screen = DisplayDevice.Default.Bounds
+    setGameWindowSize (int (float screen.Width * ratio)) (int (float screen.Height * ratio)) gameWindow
+
+let centerGameWindow (gameWindow:GameWindow) =
+    let screen = DisplayDevice.Default.Bounds
+    gameWindow.X <- (screen.Width - gameWindow.Width) / 2
+    gameWindow.Y <- (screen.Height - gameWindow.Height) / 2
+    gameWindow
+
+type Application(title) =
+    let gameWindow = new GameWindow(title)
+                    |> setGameWindowSizeScreenRatio 0.8
+                    |> centerGameWindow
+    member this.setOnUpdate = gameWindow.setOnUpdate
+    member this.run () = gameWindow.Run(30.)
