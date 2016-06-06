823d4bd6fbb5256bec558b6bbe8e126d4c206608
Antonio Maiorano <amaiorano@gmail.com>
2016-06-05 21:41:13 -0400

    Setup scaffolding for Enemy AI

--- src/Main.fs
+++ src/Main.fs
@@ -11,31 +11,60 @@ open System
 
 let swapArgs f a b = f b a
 
-type Vec = (float*float)
-type VecList = Vec list
+let cos = Math.Cos
+let sin = Math.Sin
+let cossin rads = (cos(rads), sin(rads))
+let twoPI = 2. * Math.PI
+
+
+module Vec2 =
+    type T = (float * float)
+    let add (x1,y1) (x2,y2) = (x1 + x2, y1 + y2)
+    let mul s (x,y) = (x * s, y * s)
+    let addMul (x,y) (dx,dy) s = (x + dx * s, y + dy * s)    
+    let fromAngle degs = cossin(degToRad degs)
+    let magnitude (x,y) = Math.Sqrt(x*x + y*y)
+    let normalize (x,y) =
+        let mag = magnitude (x,y)
+        assert (mag <> 0.)
+        (x / mag, y / mag)
+    let dot (x1,y1) (x2,y2) = x1*x2 + y1*y2
+    // returns rads
+    let angleDiff v1 v2 = Math.Acos (dot (normalize(v1)) (normalize(v2)))
+
+type Vec2List = Vec2.T list
 
 type Visual = {
-    vertices:VecList list
+    vertices:Vec2List list
 }
 
 type Actor = {
-    pos:Vec
+    pos:Vec2.T
     angle:float
     speed:float
     visual:Visual
 }
-
 let defaultActor = {
     pos = (0.,0.)
     angle = 0.
-    speed=0.
+    speed = 0.
     visual = { vertices = [[]] }
 }
 
-let cos = Math.Cos
-let sin = Math.Sin
-let cossin rads :Vec = (cos(rads), sin(rads))
-let twoPI = 2. * Math.PI
+type EnemyState =
+    | Idle
+    | GrabBarrel of barrel:Actor
+    | Leave
+    | AttackPlayer of player:Actor
+
+type Enemy = {
+    actor : Actor
+    state : EnemyState
+}
+let defaultEnemy = {
+    actor = defaultActor
+    state = Idle
+}
 
 let drawVisual (canvas:Canvas) (vis:Visual) =
     vis.vertices |> List.iter canvas.drawVertices
@@ -51,6 +80,7 @@ let drawActor (canvas:Canvas) (actor:Actor) =
 // e.g. [[1;2]; [3;4]] -> [1;2;3;4]
 let flattenListList (x:'a list list) = x |> List.reduce List.append
 
+
 module ShapeBuilder =
     let scale (x,y) verts = verts |> List.map(fun (x',y') -> (x'*x, y'*y))
     let scaleUni s verts = scale (s,s) verts
@@ -58,20 +88,20 @@ module ShapeBuilder =
     let translateInv (x,y) verts = translate (-x,-y) verts
 
     // returns (minX,minY,maxX,maxY)
-    let extents (v:VecList) =
-        let rec f (v:VecList) (minX,minY,maxX,maxY) =
+    let extents (v:Vec2List) =
+        let rec f (v:Vec2List) (minX,minY,maxX,maxY) =
             match v with
             | [] -> (minX,minY,maxX,maxY)
             | (hx,hy) :: tail -> f tail (Math.Min(hx,minX), Math.Min(hy,minY), Math.Max(hx,maxX), Math.Max(hy,maxY))
         f v (0.,0.,0.,0.)
 
-    // returns center point of input extents (as Vec)
-    let midpoint (v:VecList) =
+    // returns center point of input extents (as Vec2)
+    let midpoint (v:Vec2List) =
         let (minX,minY,maxX,maxY) = extents v
         (minX + (maxX-minX)/2., minY + (maxY-minY)/2.)
 
-    // returns new VecList translated so that its midpoint is (0,0) in local space
-    let center (v:VecList) = translateInv (midpoint v) v
+    // returns new Vec2List translated so that its midpoint is (0,0) in local space
+    let center (v:Vec2List) = translateInv (midpoint v) v
     
     // shape functions return unit-sized shapes centered around the origin
     let square = [ (0.,0.); (0.,1.); (1.,1.); (1.,0.) ] |> center
@@ -79,36 +109,42 @@ module ShapeBuilder =
     let triangle = circle 3
 
 
-open ShapeBuilder
+module GameVisual =
+    open ShapeBuilder
 
-let makeTankVisual () =
-    let body = square |> scale (28., 16.)
-    let turret = triangle |> scale (20., 4.) |> translate (5., 0.)
-    let buildWheels size num =
-        [for i in 0. .. (float)num-1. do yield! translate (i*size, 0.) (scaleUni size square)] |> center
-    let wheels = buildWheels 6. 5
-    let wheel1 = wheels |> translate (0., -13.)
-    let wheel2 = wheels |> translate (0., 13.)
-    { vertices = [body; wheel1; wheel2; turret] }
+    let makeTank () =
+        let body = square |> scale (28., 16.)
+        let turret = triangle |> scale (20., 4.) |> translate (5., 0.)
+        let buildWheels size num =
+            [for i in 0. .. (float)num-1. do 
+                yield! translate (i*size, 0.) (scaleUni size square)] |> center
+        let wheels = buildWheels 6. 5
+        let wheel1 = wheels |> translate (0., -13.)
+        let wheel2 = wheels |> translate (0., 13.)
+        { vertices = [body; wheel1; wheel2; turret] }
 
-let add ((x1,y1):Vec) ((x2,y2):Vec) = (x1 + x2, y1 + y2)
-let mul s (x,y) = (x * s, y * s)
-let addMul (x,y) (dx,dy) s = (x + dx * s, y + dy * s)
-let angleToVector degs = cossin(degToRad degs)
-let rotateActor degs actor = {actor with angle = actor.angle + degs}
+    let makeEnemy () =
+        let body = square |> scale (25., 16.)
+        let body2 = circle 8 |> scaleUni 5.
+        let turret = triangle |> scale (10., 4.) |> translate (12., 0.)
+        let turret1 = turret |> translate(0., -5.)
+        let turret2 = turret |> translate(0., 5.)
+        { vertices = [body; body2; turret1; turret2] }
+
+    let makeBarrel () =
+        { vertices = [circle 6 |> scaleUni 10.] }
+
+//let rotateActor degs actor = {actor with angle = actor.angle + degs}
 
 type GameData = {
     player:Actor;
     barrels:Actor list;
-    enemies:Actor list;
+    enemies:Enemy list;
 }
 
-let allActors gameData = [gameData.player] @ gameData.barrels @ gameData.enemies
+let allActors gameData = [gameData.player] @ gameData.barrels @ (gameData.enemies |> List.map (fun e -> e.actor))
 
-let rec update (app:Application) (gameData:GameData) (dt:float) (canvas:Canvas) =
-    // Logic update
-    let player = gameData.player
-    
+let movePlayer dt player =
     let turnRate = 360.
     let angleDelta =
         if Keyboard.IsDown Key.Left then turnRate
@@ -127,20 +163,31 @@ let rec update (app:Application) (gameData:GameData) (dt:float) (canvas:Canvas) 
     let finalSpeed = Math.Min(Math.Max(0., player.speed + speedDelta * dt), maxSpeed)
 
     let moveDelta = finalSpeed * dt
-    let forward = angleToVector finalAngle
-    let finalPos = addMul player.pos forward moveDelta
+    let forward = Vec2.fromAngle finalAngle
+    let finalPos = Vec2.addMul player.pos forward moveDelta
+    { player with pos = finalPos; angle = finalAngle; speed = finalSpeed }    
 
-    let player = {
-        player with
-            angle = finalAngle
-            pos = finalPos
-            speed = finalSpeed
-        }
+let updateEnemy dt (gameData:GameData) (enemy:Enemy) =
+    let enemy =
+        match enemy.state with
+        | Idle -> {enemy with state = GrabBarrel(gameData.barrels.[0])}
+        | GrabBarrel(barrel) ->
+            // Turn and move towards the barrel
+            // If we're close enough, change state to Leave
+            enemy
+        | Leave -> enemy
+        | AttackPlayer(player) -> enemy
 
-    let gameData = { gameData with player = player }
+    enemy
+
+let rec update (app:Application) (gameData:GameData) (dt:float) (canvas:Canvas) =
+    // Logic update
+    let player = gameData.player |> movePlayer dt
+    let enemies = gameData.enemies |> List.map (updateEnemy dt gameData)
 
     // To keep it pure, we need to re-register a new instance of update that
-    // binds the udpated actors list
+    // binds the updated gameData
+    let gameData = { gameData with player = player; enemies = enemies }
     app.setOnUpdate (update app gameData)
 
     // Render
@@ -148,16 +195,16 @@ let rec update (app:Application) (gameData:GameData) (dt:float) (canvas:Canvas) 
     canvas.clear ()
     allActors gameData |> List.iter (fun (actor) -> drawActor canvas actor)
 
-
 let main () =
-    let player = { defaultActor with pos = (120.,40.); angle = 120.; visual = makeTankVisual() }
-    let barrel = { defaultActor with visual = { vertices = [circle 6 |> scaleUni 10.] } }
+    let player = { defaultActor with pos = (120.,40.); angle = 120.; visual = GameVisual.makeTank() }
+    let barrel = { defaultActor with visual = GameVisual.makeBarrel() }
+    let enemy = { defaultEnemy with actor = { defaultActor with pos = (-200.0, 200.0); visual = GameVisual.makeEnemy() } }
     
     let numBarrels = 6.
     let barrelSpread = 50.
-    let barrels = [ for i in 0. .. numBarrels -> { barrel with pos = angleToVector(i * (360./numBarrels)) |> mul barrelSpread } ]
+    let barrels = [ for i in 0. .. numBarrels -> { barrel with pos = Vec2.fromAngle(i * (360./numBarrels)) |> Vec2.mul barrelSpread } ]
     
-    let gameData = { player = player; barrels = barrels; enemies = [] }
+    let gameData = { player = player; barrels = barrels; enemies = [enemy] }
     
     let app = new Application ("FSharpRipOff", (800, 600))
     app.setOnUpdate (update app gameData)
